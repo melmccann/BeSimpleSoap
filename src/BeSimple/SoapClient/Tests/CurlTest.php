ce63a0f79a7b7d111230bf936c118d39b2ba3c59
mel.mccann <mel.mccann@smartbox.com>
2018-01-25 18:00:51 +0100

    udpate curl test

--- src/BeSimple/SoapClient/Tests/CurlTest.php
+++ src/BeSimple/SoapClient/Tests/CurlTest.php
@@ -36,13 +36,13 @@ class CurlTest extends AbstractWebserverTest
         ));
 
         $curl->exec('http://unknown/curl.txt');
-        $this->assertRegExp('/^Could not connect to host.*$/', $curl->getErrorMessage());
+        $this->assertEquals('Could not connect to host', $curl->getErrorMessage());
 
         $curl->exec(sprintf('xyz://localhost:%d/@404.txt', WEBSERVER_PORT));
-        $this->assertRegExp('/^Unknown protocol. Only http and https are allowed.*$/', $curl->getErrorMessage());
+        $this->assertEquals('Unknown protocol. Only http and https are allowed.', $curl->getErrorMessage());
 
         $curl->exec('');
-        $this->assertRegExp('/^Unable to parse URL.*$/', $curl->getErrorMessage());
+        $this->assertEquals('Unable to parse URL', $curl->getErrorMessage());
     }
 
     public function testGetRequestHeaders()
