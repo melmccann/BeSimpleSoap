4ec73452800f9be58d51690580abab34ff8ef7e6
mel.mccann <mel.mccann@smartbox.com>
2018-01-26 12:53:45 +0100

    revert

--- src/BeSimple/SoapClient/Tests/CurlTest.php
+++ src/BeSimple/SoapClient/Tests/CurlTest.php
@@ -36,10 +36,10 @@ class CurlTest extends AbstractWebserverTest
         ));
 
         $curl->exec('http://unknown/curl.txt');
-        $this->assertEquals('Could not connect to host', $curl->getErrorMessage());
+        $this->assertRegExp('/^Could not connect to host.*$/', $curl->getErrorMessage());
 
         $curl->exec(sprintf('xyz://localhost:%d/@404.txt', WEBSERVER_PORT));
-        $this->assertEquals('Unknown protocol. Only http and https are allowed.', $curl->getErrorMessage());
+        $this->assertRegExp('/^Unknown protocol. Only http and https are allowed.*$/', $curl->getErrorMessage());
 
         $curl->exec('');
         $this->assertEquals('Unable to parse URL', $curl->getErrorMessage());
